52bdfa9f0c0f7aa0249745564ed7e67bd96423cd
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-12-20 12:17:34 +0000

    Initiated nextUI

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,23 @@
+import { Input } from "@nextui-org/react";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Restaurant System</h1>
+    <main className="flex min-h-screen items-center p-24">
+      <div className="flex flex-col gap-4 w-full items-center ">
+        <Input
+          type="text"
+          color="default"
+          label="Credential"
+          placeholder="user@business"
+          className="w-1/2"
+        />
+        <Input
+          type="password"
+          label="Password"
+          placeholder="Your Password"
+          className="w-1/2"
+        />
+      </div>
     </main>
   );
 }
