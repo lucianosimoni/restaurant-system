cdca00426885b37ea621a4298c3e0555063660bf
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-12-26 07:49:26 +0000

    loading optimized

--- client/app/(auth)/login/loading.tsx
+++ client/app/(auth)/login/loading.tsx
@@ -3,19 +3,19 @@ import { Button, Input, Skeleton } from "@nextui-org/react";
 export default function LoadingLogin() {
   return (
     <form className="flex flex-col gap-4 w-full items-center">
-      <Skeleton className="md:w-1/2 w-full rounded-lg">
+      <Skeleton className="md:w-1/2 w-full rounded-large">
         <Input className="md:w-1/2 w-full" />
       </Skeleton>
 
-      <Skeleton className="md:w-1/2 w-full rounded-lg">
+      <Skeleton className="md:w-1/2 w-full rounded-large">
         <Input className="md:w-1/2 w-full" />
       </Skeleton>
 
-      <Skeleton className="md:w-1/2 w-full rounded-lg">
+      <Skeleton className="md:w-1/2 w-full rounded-large">
         <Button size="lg" className="md:w-1/2 w-full" />{" "}
       </Skeleton>
 
-      <Skeleton className="md:w-1/2 w-full rounded-lg">
+      <Skeleton className="md:w-1/2 w-full rounded-large">
         <Button size="lg" className="md:w-1/2 w-full" />
       </Skeleton>
     </form>
