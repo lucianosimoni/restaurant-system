a872155f7ec6a23780503b6959e1b18f00eab02c
Luciano Simoni <lucianosimonipersonal@gmail.com>
2024-02-08 17:28:32 +0000

    change in the routing main.jsx

--- workstation/src/main.jsx
+++ workstation/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home.jsx';
 import Login from './pages/auth/Login.jsx';
@@ -11,23 +11,19 @@ import Settings from './pages/settings/Settings.jsx';
 import InitialSettings from './pages/auth/InitialSettings.jsx';
 import Logout from './pages/auth/Logout.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  { path: '/login', element: <Login /> },
-  { path: '/logout', element: <Logout /> },
-  { path: '/initial-settings', element: <InitialSettings /> },
-  { path: '/clock-in-out', element: <ClockInOut /> },
-  { path: '/settings', element: <Settings /> },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className="h-[100dvh]">
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/initial-settings" element={<InitialSettings />} />
+          <Route path="/clock-in-out" element={<ClockInOut />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Router>
     </div>
   </React.StrictMode>,
 );
