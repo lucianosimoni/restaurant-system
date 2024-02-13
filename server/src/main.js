bad8594f2d05fd34dc8cbed07e3b644b04e6cb31
Luciano Simoni <lucianosimonipersonal@gmail.com>
2024-02-13 07:10:07 +0000

    removed dotenv import from prod code & added dotenv to dev script

--- server/src/main.js
+++ server/src/main.js
@@ -1,4 +1,3 @@
-import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
@@ -14,8 +13,6 @@ import screenRouter from "./routers/screen.js";
 
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json" assert { type: "json" };
-
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
